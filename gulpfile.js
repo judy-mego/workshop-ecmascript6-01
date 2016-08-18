964d1ac09fbcb4f96068d659bbe33ea2d96783c1
Pedro Peña <pedro.jose@gigigo.com>
2016-08-18 08:51:42 +0200

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserSync = require('browser-sync').create();
 var browserify  = require('browserify');
 var watchify    = require('watchify');
 var babel       = require('babelify');
+var sourcemaps  = require('gulp-sourcemaps');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
 
 var paths = {
     src : './src',
@@ -20,3 +23,34 @@ gulp.task('serve', ['watch'], function() {
 
     gulp.watch(paths.dist+'/*').on('change', browserSync.reload);
 });
+
+function compile(watch) {
+    var bundler = watchify(browserify(paths.src+'/assets/js/app.js', { debug: true }).transform(babel));
+
+    function rebundle() {
+        bundler.bundle()
+        .on('error', function(err) { console.error(err); this.emit('end'); })
+        .pipe(source('build.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.dist+'/assets/js'));
+    }
+
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+            browserSync.reload();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
